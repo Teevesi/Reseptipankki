574eae5d84760ec583ac5cc7b5c480c9c7c4690f
unknown <touko.ristiranta@gmail.com>
2025-04-12 23:15:47 +0300

    Added user access checking

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import items
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
+def require_login():
+    if "user_id" not in session:
+        abort(403)
+
 @app.route("/")
 def index():
     all_items = items.get_items()
@@ -16,6 +20,7 @@ def index():
 
 @app.route("/new_item")
 def new_item():
+    require_login()
     return render_template("new_item.html")
 
 @app.route("/item/<int:item_id>")
@@ -27,6 +32,7 @@ def show_item(item_id):
 
 @app.route("/create_item", methods=["POST"])
 def create_item():
+    require_login()
     title = request.form["title"]
     ingredients = request.form["ingredients"]
     instructions = request.form["instructions"]
@@ -36,6 +42,7 @@ def create_item():
 
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
+    require_login()
     item = items.get_item(item_id)
     if not item:
         abort(404)
@@ -45,6 +52,7 @@ def edit_item(item_id):
 
 @app.route("/update_item", methods=["POST"])
 def update_item():
+    require_login()
     item_id = request.form["item_id"]
     item = items.get_item(item_id)
     if not item:
@@ -60,6 +68,7 @@ def update_item():
 
 @app.route("/remove_item/<int:item_id>", methods=["GET", "POST"])
 def remove_item(item_id):
+    require_login()
     item = items.get_item(item_id)
     if not item:
         abort(404)
@@ -129,6 +138,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    del session["username"]
-    del session["user_id"]
+    if "user_id" in session:
+        del session["username"]
+        del session["user_id"]
     return redirect("/")
